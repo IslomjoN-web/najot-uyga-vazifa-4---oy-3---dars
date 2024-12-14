f3a4492d784e9ecd7557ce22a6393df7afe0be3d
islomjon <islomanvarov05@gmail.com>
2024-12-14 14:20:22 +0500

    vazifa bajarildi

---
+++ main.js
@@ -0,0 +1,121 @@
+// 1 - masala
+// let k = +prompt("k sonini kiriting")
+// let n = +prompt("a sonni kirit")
+
+// for(let i = 1; i <= n; i++) {
+//     console.log(k)
+// } 
+
+// 2-masala
+// let a = +prompt("a sonini kiriting")
+// let b = +prompt("b sonni kirit")
+// let count = 0
+
+// for(let i = a; i <= b; i++) {
+//     console.log(i)
+//     count ++
+// } 
+
+// console.log("sonlari soni:" + " " + count);
+
+// 3-masala
+// let a = +prompt("a sonini kiriting")
+// let b = +prompt("b sonni kirit")
+// let count = 0
+
+// for(let i = a; i >= b; i--) {
+//     console.log(i)
+//     count ++
+// } 
+// console.log("sonlari soni:" + " " + count);
+
+// 4-masala
+
+// let narx = +prompt("kanfetning 1kgni narxini kiriting...")
+
+// for(let i = 1; i <= 10; i++ ) {
+//     console.log(i * narx);
+// }
+
+
+// 5-masala
+
+// let narx = +prompt("kanfetning 1kgni narxini kiriting...")
+
+// for(let i = 0.1; i <= 1; i+=0.1 ) {
+//     console.log(narx * i);
+// }
+
+
+
+// 6-masala
+// let narx = +prompt("kanfetning 1kgni narxini kiriting...")
+
+// for(let i = 1.2; i <= 2 ; i += 0.2 ) {
+//         console.log((i.toFixed(1) * narx))
+// }
+
+// 7-masala
+
+// let a = +prompt("a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+// let yigindi = 0
+
+// for(let i = a; i <= b; i++) {
+//     yigindi = yigindi + i
+// }
+// console.log(yigindi);
+
+// 8-masala
+
+// let a = +prompt("a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+// let yigindi = 1
+
+// for(let i = a; i <= b; i++) {
+//     yigindi = yigindi * i
+// }
+// console.log(yigindi);
+
+// 9 - masala
+// let a = +prompt("a sonni kiriting")
+// let b = +prompt("b sonni kiriting")
+// let yigindi = 0
+
+// for (let i = a; i <= b; i++) {
+//     yigindi = yigindi + (i ** 2)
+// }
+// console.log(yigindi);
+
+// 10-masala
+
+// let n = +prompt("n sonni kiriting")
+// let hisob = 0
+
+
+// for(let i = 1; i <= n; i++ ) {
+//     hisob += 1/i
+// }
+
+
+// console.log(`hisob = ${hisob.toFixed(4)}`);
+
+// 11 - masala
+
+
+// let n = 3;
+// calculateSum(n);
+// function calculateSum(n) {
+//     if (n <= 0) {
+//         console.log("n musbat butun son bo'lishi kerak!");
+//         return;
+//     }
+
+//     let S = 0;
+//     for (let i = n; i <= 2 * n; i++) {
+//         S += i ** 2; 
+//     }
+//     console.log(`S = ${S}`); 
+// }
+
+
